4dbea36bd8d11275ea1645bfc862eb3ee9996a93
John Fu <fuqcool@gmail.com>
2016-04-28 17:28:31 -0400

    build ui with redux

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,10 +1,9 @@
 import gulp from 'gulp'
 import eslint from 'gulp-eslint'
 
-
 gulp.task('lint', () => {
-	return gulp.src(['./lib/**/*.js', './*.js'])
-		.pipe(eslint())
-		.pipe(eslint.format())
-		.pipe(eslint.failAfterError())
+  return gulp.src(['./lib/**/*.js', './*.js'])
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError())
 })
