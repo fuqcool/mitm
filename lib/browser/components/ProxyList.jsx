e311ae3ec1d5113e09324bdb9d8316cd4285f859
John Fu <fuqcool@gmail.com>
2016-05-13 15:24:33 -0400

    js -> jsx

--- lib/browser/components/ProxyList.jsx
+++ lib/browser/components/ProxyList.jsx
@@ -2,15 +2,15 @@ import React, {PropTypes} from 'react'
 import ProxyContainer from '../containers/ProxyContainer'
 
 const ProxyList = ({proxies}) => (
-  <div className="list-group">
-    {proxies.map((proxy) => (
-      <ProxyContainer key={proxy.id} proxy={proxy} />
-    ))}
-  </div>
+    <div className="list-group">
+      {proxies.map((proxy) => (
+          <ProxyContainer key={proxy.id} proxy={proxy} />
+      ))}
+    </div>
 )
 
 ProxyList.propTypes = {
-  proxies: PropTypes.array.isRequired
+    proxies: PropTypes.array.isRequired
 }
 
 export default ProxyList
